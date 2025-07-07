e2c7eaf1eeb3116940d50e11fab5cb97b6c176ac
Gyaneshwar Singh Bais <gyan.singh.bais@gmail.com>
2025-07-07 15:04:12 +0530

    CoverStatusDetail changes in StockMgmController

--- HIMIS_API/Controllers/StockMgmController.cs
+++ HIMIS_API/Controllers/StockMgmController.cs
@@ -102,8 +102,8 @@ select t.tender_id,ti.item_ID ,categoryName,m.item_code,m.item_Name,m.estimated_
 convert(varchar,t.ENDDate,105) as ENDDate,convert(varchar,t.cover_a,105) as cover_a,convert(varchar,t.cover_b,105)  as cover_b
 ,ct.CSID,ct.CStatus,t.tValue  
 from tenders t
-inner join tender_items ti on ti.tender_id=t.tender_id
-inner join masitems m on m.item_id=ti.item_id
+left outer join tender_items ti on ti.tender_id=t.tender_id
+left outer join masitems m on m.item_id=ti.item_id
 left outer join masCategory c on c.categoryId=m.categoryId
 inner join mascoverstatus ct on ct.CSID=t.CSID
 where ct.CSID not in (6) 
@@ -118,7 +118,7 @@ inner join  TENDERSTATUSREMARK tr on tr.tender_id=A.tender_id and ISNEW='Y'
 inner join TENDERSTATUSMASTER t  on t.tsid=tr.tsid
 where ISNEW='Y'
 )ts on ts.tender_id=x.tender_id
-where 1=1"+ whCsid + @"
+where 1=1" + whCsid + @"
 group by CSID,ts.tender_id,isGemTender,tender_no,tender_description,tender_date,ENDDate,cover_a,cover_b,CStatus,ts.TENDERSTATUS,ts.tenderremark,ts.entrydate  ";
 
             var result = await _context.CoverStatusDetailDbSet
